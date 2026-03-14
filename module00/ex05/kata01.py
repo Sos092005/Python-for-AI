39b274d5f14e555312da3d654f866540e74d6239
Sos092005 <imeriusadounsa2023@gmail.com>
2026-03-14 21:46:43 +0100

    f strings litteral, decimal , float , and scientific display

---
+++ module00/ex05/kata01.py
@@ -0,0 +1,11 @@
+# Put this at the top of your kata01.py file
+kata = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+
+
+for i,j in kata.items():
+	print(f"{i} was created by {j}")
+
---
+++ module00/ex05/kata02.py
@@ -0,0 +1,4 @@
+# Put this at the top of your kata02.py file
+kata = (2019, 9, 25, 3, 30)
+
+print(f"{kata[1]:2d}/{kata[2]:2d}/{kata[0]} {kata[3]:2d}:{kata[4]}")
